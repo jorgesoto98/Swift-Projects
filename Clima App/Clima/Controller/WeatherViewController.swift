c6dbc81096ee3198909fb8315848960eb9679b8d
jorgesoto98 <jorgesoto98@users.noreply.github.com>
2020-08-04 21:03:49 -0500

    Initial Clima App Commit

---
+++ Clima App/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Jorge Soto on 04/08/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clima App/Clima/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by Jorge Soto on 04/08/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
